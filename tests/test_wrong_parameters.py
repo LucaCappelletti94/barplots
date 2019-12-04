5fbb28ab749484b06a1b543a1f04828459082cfa
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-12-04 15:01:40 +0100

    Extended testing

--- tests/test_wrong_parameters.py
+++ tests/test_wrong_parameters.py
@@ -5,6 +5,15 @@ from barplots import barplots
 def test_wrong_parameters():
     root = "test_barplots"
     df = pd.read_csv("tests/test_case.csv")
+    
+    with pytest.raises(ValueError):
+        barplots(
+            df[df.cell_line == "CIAO"],
+            ["cell_line","task","balancing","model"],
+            orientation="pinco",
+            path="{root}/{{feature}}.jpg".format(root=root)
+        )
+
     with pytest.raises(ValueError):
         barplots(
             df,
