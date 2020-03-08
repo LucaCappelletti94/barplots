37af55948ebeb726e8fd8c9564da238ec3cbd1e3
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-03-08 16:39:59 +0100

    Resolved code smell

--- barplots/utils/text_positions.py
+++ barplots/utils/text_positions.py
@@ -14,7 +14,7 @@ def text_positions(df: pd.DataFrame, bar_width: float, index_level: int) -> Gene
         if not isinstance(index, (list, tuple)):
             index = (index,)
         jumps = get_jumps(index, old_index)
-        if jumps[index_level]:
+        if jumps[index_level] and old_index is not None:
             yield (previous_jump_position + last_position)/2, old_index[index_level]
             previous_jump_position = position[0] - bar_width/2
 
