085da4dad0aa0a941f61d28f99da22163de0ceca
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-05-30 20:13:06 +0200

    Added automatic dispatching of legend columns number

--- barplots/utils/remove_duplicated_legend_labels.py
+++ barplots/utils/remove_duplicated_legend_labels.py
@@ -1,6 +1,7 @@
 from matplotlib.axes import Axes
 from sanitize_ml_labels import sanitize_ml_labels
 from typing import Dict, List
+import math
 
 
 def remove_duplicated_legend_labels(
@@ -23,13 +24,22 @@ def remove_duplicated_legend_labels(
         The defaults for normalizing the provided keys.
     """
     handles, labels = axes.get_legend_handles_labels()
+    
     by_label = dict(zip(labels, handles))
+    length__of_padding = 6
+    mean_label_length = sum(
+        len(label) for label in by_label.keys()
+    ) / len(by_label) + length__of_padding
+
+    ncol = math.ceil(len(legend_title) / mean_label_length)
+
     legend = axes.legend(
         by_label.values(),
         sanitize_ml_labels(
             by_label.keys(),
             custom_defaults=custom_defaults
         ),
+        ncol=ncol,
         prop={'size': 9},
         loc=legend_position
     )
